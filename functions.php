12488f699e29425b7b903f28430631189c27ddc7
Grant Darling <grantcarterdarling@gmail.com>
2021-10-07 18:15:45 -0400

    cleaned up footer and functions.php

--- functions.php
+++ functions.php
@@ -7,10 +7,13 @@
  * @package The_Code_Bytes_2021
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
-}
+/**
+ * Includes Sections 
+ */
+
+include 'shortcodes.php';
+
+if ( ! defined( '_S_VERSION' ) ) { define( '_S_VERSION', '1.0.0' ); }
 
 if ( ! function_exists( 'the_code_bytes_2021_setup' ) ) :
 	/**
@@ -21,30 +24,13 @@ if ( ! function_exists( 'the_code_bytes_2021_setup' ) ) :
 	 * as indicating support for post thumbnails.
 	 */
 	function the_code_bytes_2021_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on The Code Bytes 2021, use a find and replace
-		 * to change 'the-code-bytes-2021' to the name of your theme in all the template files.
-		 */
 		load_theme_textdomain( 'the-code-bytes-2021', get_template_directory() . '/languages' );
 
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
 		add_theme_support( 'title-tag' );
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
 		add_theme_support( 'post-thumbnails' );
 
 		// This theme uses wp_nav_menu() in one location.
@@ -54,10 +40,6 @@ if ( ! function_exists( 'the_code_bytes_2021_setup' ) ) :
 			)
 		);
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
 		add_theme_support(
 			'html5',
 			array(
@@ -86,11 +68,6 @@ if ( ! function_exists( 'the_code_bytes_2021_setup' ) ) :
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
 		add_theme_support(
 			'custom-logo',
 			array(
@@ -228,30 +205,12 @@ function getArticlesByTag($tagName, $postsPerPage, $articleType) {
 	}
 
 /**
- * SQL Query
- */
-
-function absoluteGarbageSQLQuery() {
-	global $wpdb;
-	$retrieve_data = $wpdb->get_results( "SELECT * FROM wp_posts LIMIT 10");
-	foreach ($retrieve_data as $retrieved_data){ 
-	echo '<div> ' . $retrieved_data->post_content . ' </div>';
-	}
-}
-
-/**
  * Generic Newsletter 
  */
 
  function newsletter() {
 	 echo do_shortcode('[newsletter]');
  }
-
-/**
- * Code Review
- */
-
-include 'shortcodes.php';
 
 
 
