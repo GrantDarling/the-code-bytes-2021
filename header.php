f16938285b3868c20594a0739a9eeacc993a3a20
Grant Darling <grantcarterdarling@gmail.com>
2021-10-07 18:21:46 -0400

    updated header.php

--- header.php
+++ header.php
@@ -16,41 +16,47 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+
 	<!-- Adsense Ads -->
 	<script data-ad-client="ca-pub-3304193651719804" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<script async custom-element="amp-auto-ads"
-			src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js">
-	</script>
+	<script async custom-element="amp-auto-ads" src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"></script>
+
 	<?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+<body 
+	<?php body_class(); ?>>
+	<?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'the-code-bytes-2021' ); ?></a>
 
 	<header id="masthead" class="site-header">
+	  
 		<div id="header-container">
-			<div class="site-branding">
-				<?php
-				the_custom_logo();
-				if ( is_front_page() && is_home() ) :
-					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
-				else :
-					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-				endif;
 
-				$the_code_bytes_2021_description = get_bloginfo( 'description', 'display' );
+				<!-- Site Branding Container -->
+			<div class="site-branding">
+				<?php 
+					the_custom_logo();
+					if ( is_front_page() && is_home() ) : ?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+					else :
+				?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+					endif;
+
+					$the_code_bytes_2021_description = get_bloginfo( 'description', 'display' );
 				
-				if ( $the_code_bytes_2021_description || is_customize_preview() ) :
-					?>
+					if ( $the_code_bytes_2021_description || is_customize_preview() ) :
+				?>
 					<p class="site-description"><?php echo $the_code_bytes_2021_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-				<?php endif; ?>
-			</div><!-- .site-branding -->
-			
+				<?php 
+					endif; 
+				?>
+			</div>
+
+			<!-- Navigation -->
 			<nav id="site-navigation" class="main-navigation">
 				<div class="navigation-header">
 				<h1 id="mobile-title" aria-controls="primary-menu">THE CODE BYTES</h1>
@@ -67,11 +73,9 @@
 						'menu_id'        => 'primary-menu',
 					)
 				);
-				// Search bar 
 				get_search_form();
 				?>
-			</nav><!-- #site-navigation -->
+			</nav>
 		</div>
 	</header>
 	<div class="header-shim"></div>
-	<!-- #masthead -->
