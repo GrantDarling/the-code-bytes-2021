63ddd3ed6ad23c1b044edab9eb6461588eb139ba
Grant Darling <grantcarterdarling@gmail.com>
2021-10-07 18:07:37 -0400

    cleaned up comments.php

--- comments.php
+++ comments.php
@@ -10,68 +10,57 @@
  * @package The_Code_Bytes_2021
  */
 
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
-if ( post_password_required() ) {
-	return;
-}
 ?>
 
-<div id="comments" class="comments-area">
+<?php if ( post_password_required() ) { return; } ?>
 
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
+<div id="comments" class="comments-area">
+	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
 			$the_code_bytes_2021_comment_count = get_comments_number();
 			if ( '1' === $the_code_bytes_2021_comment_count ) {
 				printf(
-					/* translators: 1: title. */
 					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'the-code-bytes-2021' ),
 					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
 				);
 			} else {
 				printf( 
-					/* translators: 1: comment count number, 2: title. */
 					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $the_code_bytes_2021_comment_count, 'comments title', 'the-code-bytes-2021' ) ),
 					number_format_i18n( $the_code_bytes_2021_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
 				);
 			}
 			?>
-		</h2><!-- .comments-title -->
+		</h2>
 
 		<?php the_comments_navigation(); ?>
 
 		<ol class="comment-list">
 			<?php
-			wp_list_comments(
-				array(
-					'style'      => 'ol',
-					'short_ping' => true,
-				)
-			);
+				wp_list_comments(
+					array(
+						'style'      => 'ol',
+						'short_ping' => true,
+					)
+				);
 			?>
-		</ol><!-- .comment-list -->
+		</ol>
 
 		<?php
 		the_comments_navigation();
 
-		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() ) :
 			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'the-code-bytes-2021' ); ?></p>
+			<p class="no-comments">
+				<?php esc_html_e( 'Comments are closed.', 'the-code-bytes-2021' ); ?>
+			</p>
 			<?php
 		endif;
 
-	endif; // Check for have_comments().
+	endif;
 
 	comment_form();
 	?>
 
-</div><!-- #comments -->
+</div>
